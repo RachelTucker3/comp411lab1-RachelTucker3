bcfac2764b902e803f083a4059edc8bb123c4129
Rachel Tucker <rach1234@live.unc.edu>
2019-04-26 16:07:26 +0000

    replace this with a useful message

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define PI 3.14159
+
+int main()
+{
+	float r, a;
+
+	printf("Enter radius (in cm):\n");
+	scanf("%f", &r);
+	a = PI * (r/2.54) * (r/2.54);
+	printf("Circle's area is %3.2f (sq in).\n", a);
+}
